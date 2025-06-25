11627cf1067987736fbf875c1acd4db5bac0bbfb
Igor Podsiadły <igor-podsiadly23@wp.pl>
2025-06-25 21:58:39 +0200

    Rozwiązanie zadania2 v3

--- zadanie2.sh
+++ zadanie2.sh
@@ -3,5 +3,6 @@ unzip lista.zip
 dos2unix lista.txt lista-pop.txt
 diff lista.txt lista-pop.txt | less
 diff -u lista.txt lista-pop.txt > lista.patch
-
-
+patch lista.txt < lista.patch
+md5sum lista.txt
+md5sum lista-pop.txt
