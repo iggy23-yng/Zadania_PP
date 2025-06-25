6b4717bfb33a017f0a47e314531258c2c6629a74
Igor Podsiadły <igor-podsiadly23@wp.pl>
2025-06-25 22:32:16 +0200

    git commit zadanie4.sh

--- zadanie1.sh
+++ zadanie1.sh
@@ -1,4 +1,4 @@
 #Zadanie_1
 unzip dane.zip
 dos2unix dane.txt
-echo -e "x\ty\tz" > danexyz.txt && paste - - - < dane.txt >> danexyz.txt 
+echo -e "x\ty\tz" > danexyz.txt && paste - - - < dane.txt >> danexyz.txt
