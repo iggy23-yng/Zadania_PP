6215df8966dfa7b641d524490dbf1592b64d230b
Igor Podsiadły <igor-podsiadly23@wp.pl>
2025-06-25 21:31:31 +0200

    usunięto zadanie1

--- zadanie1.sh
+++
@@ -1,9 +0,0 @@
-# Rozpakowanie katalogu .zip
-unzip dane.zip
-
-# konwersja końca linii
-dos2unix dane.txt
- 
-# Tworzenie pliku z nagłówkiem i podziałem na kolumny
-echo -e "x\ty\tz" > danexyz.txt && paste - - - < dane.txt >> danexyz.txt 
-
