6f6a6a19e7bcb39816adba68acb1df3a924d7c64
Igor Podsiadły <igor-podsiadly23@wp.pl>
2025-06-26 11:48:40 +0200

    Fotograf gamoń

--- zadanie5.sh
+++ zadanie5.sh
@@ -4,3 +4,6 @@ unzip kopie-2.zip -d fotografie
 cd fotografie
 unzip '*.zip' -d ../rozpakowane_foto
 cd ..
+magick mogrify -format jpg rozpakowane_foto/*.png
+magick mogrify -resize x720 -density 96x96 rozpakowane_foto/*.jpg 
+zip archiwum_foto rozpakowane_foto/*.jpg
