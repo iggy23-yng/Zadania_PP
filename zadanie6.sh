42d63435b19556552b3e7ca2a959da2555e8880b
Igor Podsiadły <igor-podsiadly23@wp.pl>
2025-06-26 21:15:53 +0200

    Wszędzie te PDF-y

--- zadanie6.sh
+++ zadanie6.sh
@@ -1,4 +1,8 @@
 #Wszędzie_te_PDF-y
-cd rozpakowane_foto
-ls | shuf | head -8 > ../portfolio
+mkdir -p portfolio
+cd rozpakowane
+ls *.jpg | shuf | head -8 | while read file; do cp "$file" ../portfolio/; done
+cd ..
+magick montage -label %f portfolio/*.jpg -tile 2x4 -geometry 1235x870+5+5 -pointsize 20 portfolio.jpg
+magick portfolio.jpg -density 300 -resize 2480x3508 -gravity center -extent 2480x3508 portfolio_a4.pdf
 
